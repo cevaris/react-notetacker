baee39f202ac2dc7a2f8cd0707247897a3155cbd
Adam Cardenas <acardenas@twitter.com>
2016-03-20 17:43:32 -0600

    passed state to childen components

---
+++ app/components/notes/Notes.js
@@ -0,0 +1,17 @@
+var React = require('react');
+
+var Notes = React.createClass({
+
+  render: function () {
+    return (
+      <div>
+        <p>
+          Notes: {this.props.notes}
+        </p>
+      </div>
+    )
+  }
+
+});
+
+module.exports = Notes;
