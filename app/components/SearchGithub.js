d996812f1c491903df89d9a63360c3bce29ddd2d
Adam Cardenas <acardenas@twitter.com>
2016-03-27 09:38:06 -0600

    updated deps; added github user searchbox

---
+++ app/components/SearchGithub.js
@@ -0,0 +1,34 @@
+var React = require('react');
+var Router = require('react-router');
+
+var SearchGithub = React.createClass({
+  mixins: [Router.History],
+  getRef: function (ref) {
+    this.usernameRef = ref;
+  },
+  daSubmit: function (e) {
+    var username = this.usernameRef.value;
+    this.usernameRef.value = '';
+    if(username.trim()){
+      this.history.pushState(null, "profile/" + username);
+    }
+    e.preventDefault();
+  },
+  render: function () {
+    return (
+      <div className="col-sm-12">
+        <form onSubmit={this.daSubmit}>
+          <div className="form-group col-sm-7">
+            <input type="text" className="form-control" ref={this.getRef}/>
+          </div>
+          <div className="form-group col-sm-5">
+            <button type="submit" className="btn btn-block btn-primary">Search GitHub</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+});
+
+
+module.exports = SearchGithub;
